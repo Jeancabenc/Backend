7749922efd3659cb3361ff52dab9c3f9fb6d0dd3
Jeancabenc <jeanca.benc@gmail.com>
2025-04-11 12:58:56 -0400

    Add gunicorn to requirements.txt

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ app = Flask(__name__)
 def get_fights():
     url = 'https://boxing-data-api.p.rapidapi.com/v1/events/schedule?days=30&past_hours=12&date_sort=ASC&page_num=1&page_size=25'
     headers = {
-        'x-rapidapi-host': 'boxing-data-api.p.rapidapi.com',
-        'x-rapidapi-key': '70056ade0bmsh6870ea67c8dc851p167b8djsncbf0b288f605'
+        'x-rapidapi-host: boxing-data-api.p.rapidapi.com',
+        'x-rapidapi-key: 70056ade0bmsh6870ea67c8dc851p167b8djsncbf0b288f605'
     }
     response = requests.get(url, headers=headers)
     data = response.json()
+
+    if response.status_code != 200:
+        return jsonify({'error': 'No se pudo obtener los datos de la API externa.'}), 500
+
+    data = response.json()
+    if 'events' not in data or not data['events']:
+        return jsonify({'error': 'No hay peleas disponibles en la API.'}), 404
 
     fights = []
     for event in data.get('events', []):
